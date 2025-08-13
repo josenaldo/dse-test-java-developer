8413b0544b0036969bae77fe4f637f00d308a33d
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2025-08-13 12:04:20 -0300

    feat: add input validation to AnagramGenerator
    - Added null and empty checks for input in `AnagramGenerator`.
    - Updated `AnagramGeneratorTest` with test cases for validation:
      - Null and empty set handling.
      - Special characters and numeric input restrictions.
      - Single-character input validation.

--- src/main/java/br/com/josenaldo/AnagramGenerator.java
+++ src/main/java/br/com/josenaldo/AnagramGenerator.java
@@ -1,9 +1,13 @@
 package br.com.josenaldo;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class AnagramGenerator {
   public String[] generate(Set<Character> letters) {
+    Objects.requireNonNull(letters, "Set of characters cannot be null");
+
+
     return new String[0];
   }
 }
--- src/test/java/br/com/josenaldo/AnagramGeneratorTest.java
+++ src/test/java/br/com/josenaldo/AnagramGeneratorTest.java
@@ -3,6 +3,7 @@ package br.com.josenaldo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Set;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AnagramGeneratorTest {
@@ -21,4 +22,93 @@ class AnagramGeneratorTest {
     assertThat(generate).containsExactlyInAnyOrder(expected);
   }
 
+  @Test
+  void givenAnEmptySetOfCharacters_whenGenerate_thenThrowAnIllegalArgumentException(){
+    // Arrange - Given
+    final Set<Character> letters = Set.of();
+    final var expected = new IllegalArgumentException("Set of characters cannot be empty");
+
+    // Act - When
+    final var anagramGenerator = new AnagramGenerator();
+    final var actualException = Assertions.catchIllegalArgumentException(() -> anagramGenerator.generate(letters));
+
+    // Assert - Then
+    assertThat(actualException).hasMessage(expected.getMessage());
+    assertThat(actualException).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void givenAnNullSetOfCharacters_whenGenerate_thenThrowAnNullPointerException(){
+    // Arrange - Given
+    final Set<Character> letters = null;
+    final var expected = new NullPointerException("Set of characters cannot be null");
+
+    // Act - When
+    final var anagramGenerator = new AnagramGenerator();
+    final var actualException = Assertions.catchNullPointerException(() -> anagramGenerator.generate(letters));
+
+    // Assert - Then
+    assertThat(actualException).hasMessage(expected.getMessage());
+    assertThat(actualException).isInstanceOf(NullPointerException.class);
+  }
+
+  @Test
+  void givenASetOfCharactersWithEmptySpace_whenGenerate_thenThrowAnIllegalArgumentException(){
+    // Arrange - Given
+    final Set<Character> letters = Set.of(' ');
+    final var expected = new IllegalArgumentException("Set of characters cannot be empty");
+
+    // Act - When
+    final var anagramGenerator = new AnagramGenerator();
+    final var actualException = Assertions.catchIllegalArgumentException(() -> anagramGenerator.generate(letters));
+
+    // Assert - Then
+    assertThat(actualException).hasMessage(expected.getMessage());
+    assertThat(actualException).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void givenASetOfCharactersWithNumbers_whenGenerate_thenThrowAnIllegalArgumentException(){
+    // Arrange - Given
+    final Set<Character> letters = Set.of('a', '2', 'c');
+    final var expected = new IllegalArgumentException("Set of characters can only contain letters");
+
+    // Act - When
+    final var anagramGenerator = new AnagramGenerator();
+    final var actualException = Assertions.catchIllegalArgumentException(() -> anagramGenerator.generate(letters));
+
+    // Assert - Then
+    assertThat(actualException).hasMessage(expected.getMessage());
+    assertThat(actualException).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void givenASetOfCharactersWithSpecialCharacters_whenGenerate_thenThrowAnIllegalArgumentException(){
+    // Arrange - Given
+    final Set<Character> letters = Set.of('a', '#', 'c');
+    final var expected = new IllegalArgumentException("Set of characters can only contain letters");
+
+    // Act - When
+    final var anagramGenerator = new AnagramGenerator();
+    final var actualException = Assertions.catchIllegalArgumentException(() -> anagramGenerator.generate(letters));
+
+    // Assert - Then
+    assertThat(actualException).hasMessage(expected.getMessage());
+    assertThat(actualException).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void givenASetOfCharactersWithSingleCharacter_whenGenerate_thenThrowAnIllegalArgumentException(){
+    // Arrange - Given
+    final Set<Character> letters = Set.of('a');
+    final var expected = new IllegalArgumentException("Set of characters must contain more than one character");
+
+    // Act - When
+    final var anagramGenerator = new AnagramGenerator();
+    final var actualException = Assertions.catchIllegalArgumentException(() -> anagramGenerator.generate(letters));
+
+    // Assert - Then
+    assertThat(actualException).hasMessage(expected.getMessage());
+    assertThat(actualException).isInstanceOf(IllegalArgumentException.class);
+  }
 }
